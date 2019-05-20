e4d1585680e76a0251f41d0f6e76e201527c2ccd
Frazzer951 <luke343279@gmail.com>
2019-05-20 14:13:13 -0700

    Update person.java

--- person.java
+++ person.java
@@ -3,13 +3,62 @@
  */
 public class Person {
 
+    // Declares the Variables
     private double income;
     private String name;
     private double bal;
 
+    //Constructor for the Person Class
     public void person(String name, double bal, double income) {
         this.name = name;
         this.bal = bal;
         this.income = income;
+        int health = 100;
+
+    }
+
+    //Adds Money to Balance
+    public void addMoney(double x) {
+        bal += x;
+    }
+
+    //Removes Money from Balance
+    public void removeMoney(double x) {
+        bal -= x;
+    }
+
+    //Adds Health
+    public void addHealth(int x) {
+        health += x;
+    }
+
+    //Removes Health
+    public void removeHealth(int x) {
+        health -= x;
+    }
+
+    //Changes the Income
+    public void changeIncome(double x) {
+        income = x;
+    }
+
+    //Adds the Income value to the Balance
+    public void payday() {
+        bal += income;
+    }
+
+    //Returnes the Balance
+    public double getBal() {
+        return bal;
+    }
+
+    //Returns the Health
+    public int getHealth() {
+        return health;
+    }
+
+    //Returns the Income
+    public double getIncome() {
+        return income;
     }
 }
