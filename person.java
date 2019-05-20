ff9c434e79766a5113b8320e0d99c32933ee59d3
Frazzer951 <luke343279@gmail.com>
2019-05-20 13:58:54 -0700

    Person Class Update
    i add constructor

--- person.java
+++ person.java
@@ -2,6 +2,16 @@
  * person
  */
 public class person {
+public class Person {
 
     
+    private double income;
+    private String name;
+    private double bal;
+
+    public void person(String name, double bal, double income) {
+        this.name = name;
+        this.bal = bal;
+        this.income = income;
+    }
 }
