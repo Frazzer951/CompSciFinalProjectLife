e1deb3a2c4b45089b1633849117c58167b8bf840
stalebread72 <50841533+stalebread72@users.noreply.github.com>
2019-05-20 15:47:21 -0700

    Update Story.java

--- Story.java
+++ Story.java
@@ -1,7 +1,29 @@
 /**
  * Story
  */
+// Add an "end" factor to the game"
+private static int yearsOld = 0;
+private double goodChoice = 0.7;
+
 public class Story {
 
     
 }
+priavte void gameOver() {
+    if(yearsOld >= 100)
+    {
+        break;
+    }
+}
+
+private void daysDecision()
+{
+    if(blah blah blah)
+    {
+        lead to a delimma method
+    }
+}
+
+    
+    
+   
