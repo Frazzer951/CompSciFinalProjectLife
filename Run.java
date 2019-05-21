2e430f3e64094ed5b254fb39c850c40918b48c20
Frazzer951 <luke343279@gmail.com>
2019-05-20 20:21:29 -0700

    Update Formate and Story
    also added get name funciton

--- Run.java
+++ Run.java
@@ -1,3 +1,4 @@
+
 /**
  * Run
  */
@@ -6,12 +7,12 @@ import java.util.Scanner;
 public class Run {
 
     public static void main(String[] args) {
-        //Gets User Input for Name
+        // Gets User Input for Name
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter your name : ");
         String s = input.next(); // getting a String value
 
-        //Creates Person with name starting with 0 cash and 0 income
+        // Creates Person with name starting with 0 cash and 0 income
         Person p = new Person(s, 0.0, 0.0);
     }
 }
--- Story.java
+++ Story.java
@@ -2,29 +2,15 @@
  * Story
  */
 
-// Add an "end" factor to the game"
-private static int yearsOld = 0;
-private double goodChoice = 0.7;
-
 public class Story {
 
-    
-}
-priavte void gameOver() {
-    if(yearsOld >= 100)
-    {
-        break;
-    }
-}
+    private static int yearsOld = 0;
 
-private void daysDecision()
-{
-    if(blah blah blah)
-    {
-        lead to a delimma method
+    private boolean gameOver(int health) {
+        if (yearsOld >= 100 || health <= 0) {
+            return true;
+        }
+        return false;
     }
-}
 
-    
-    
-   
+}
